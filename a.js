76215fb29cca77db0b0731b2cf83fad43cd14efc
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2024-02-12 17:16:44 -0700

    a

--- a.js
+++ a.js
@@ -2,5 +2,35 @@ const add = () =>{
     var num1 = parseFloat(document.getElementById('num1').value)
     var num2 = parseFloat(document.getElementById('num2').value)    
     var result = num1 + num2
-    document.getElementById('result').innerHTML = 'Result: ' + result
+    document.getElementById('result').innerHTML = result
+}
+const sub = () =>{
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var result = num1 - num2
+    document.getElementById('result').innerHTML = result
+}
+const mult = () =>{
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var result = num1 * num2
+    document.getElementById('result').innerHTML = result
+}
+const div = () =>{
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var result = num1 / num2
+    document.getElementById('result').innerHTML = result
+}
+const sqrt = () =>{
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = 0.5
+    var result = num1 * num2
+    document.getElementById('result').innerHTML = result
+}
+const power = () =>{
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var result = num1 ** num2
+    document.getElementById('result').innerHTML = result
 }
