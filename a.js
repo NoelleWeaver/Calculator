6c53b93199f7696dcb2d5592b7e13c057439d021
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2024-02-13 17:50:14 -0700

    a

--- a.js
+++ a.js
@@ -1,7 +1,9 @@
+const screen = () =>{}
 const add = () =>{
     var num1 = parseFloat(document.getElementById('num1').value)
     var num2 = parseFloat(document.getElementById('num2').value)    
     var result = num1 + num2
+    document.getElementById('result').innerHTML = 'Result: ' + result
     document.getElementById('result').innerHTML = result
 }
 const sub = () =>{
@@ -28,9 +30,27 @@ const sqrt = () =>{
     var result = num1 * num2
     document.getElementById('result').innerHTML = result
 }
-const power = () =>{
-    var num1 = parseFloat(document.getElementById('num1').value)
-    var num2 = parseFloat(document.getElementById('num2').value)
-    var result = num1 ** num2
-    document.getElementById('result').innerHTML = result
-}
+// const power = () =>{
+//     var num1 = parseFloat(document.getElementById('num1').value)
+//     var num2 = parseFloat(document.getElementById('num2').value)
+//     var result = num1 ** num2
+//     document.getElementById('result').innerHTML = result
+// }
+// const sin = () =>{
+//     var num1 = parseFloat(document.getElementById('num1').value)
+//     var result = Math.sin(num1)
+//     document.getElementById('result').innerHTML = result
+// }
+// const cos = () => {
+//     var num1 = parseFloat(document.getElementById('num1').value)
+//     var result = Math.cos(num1)
+//     document.getElementById('result').innerHTML = result
+// }
+// const tan = () => {
+//     var num1 = parseFloat(document.getElementById('num1').value)
+//     var result = Math.tan(num1)
+//     document.getElementById('result').innerHTML = result
+// }
+function clr() { 
+    document.getElementById("result").value = "" 
+} 
